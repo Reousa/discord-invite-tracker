56fe4d2a6fe7d9191fef5dab6ee4a56cb9e081b5
Karim Hamdy <karim.essam.hamdy@gmail.com>
2018-01-25 18:15:20 +0200

    Update

---
+++ server.js
@@ -0,0 +1,31 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const config = require("./config.json");
+
+
+client.on('ready', () => {
+  console.log('I am ready!');
+});
+
+client.on('message', message => {
+//Ignore bot messages.
+    if(message.author.bot) return;
+//Ignore message if it doesnt start with prefix
+    if(message.content.indexOf(config.prefix) !== 0) return;
+
+    console.log("Recieved message: " + message.content);
+
+    const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
+    const command = args.shift().toLowerCase();
+
+    console.log("Arguments:" + args);
+    console.log("Command:" + command);
+
+  if (message.content === 'ping') {
+    // message.reply('pong');
+  }
+});
+
+//DO NOT SHARE THIS TOKEN PUBLICLY!!
+var token = TOKEN_VAR;
+client.login(token);
